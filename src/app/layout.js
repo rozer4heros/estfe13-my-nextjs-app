20b98a56dabea7cbf04787eb9c79619d5be4a192
rozer4heros <rozer4heros@gmail.com>
2026-07-20 14:52:55 +0900

    feat: add Controls component for topic management with Create, and Delete functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,10 @@
-// "use client";
-// import { useEffect, useState } from "react";
-
 import Link from "next/link";
 import Script from "next/script";
 
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Controls from "./Controls";
 
 export const metadata = {
   title: "웹 언어",
@@ -14,28 +13,12 @@ export const metadata = {
 
 export default async function RootLayout({ children }) {
   console.log("공통 레이아웃 작동");
-  /*
-  CLIENT COMPONENT EXCLUSIVE
-
-  const [topics, setTopics] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:9999/topics")
-      .then((response) => response.json())
-      .then((result) => {
-        setTopics(result);
-      });
-  }, []);
-  */
   const response = await fetch("http://localhost:9999/topics", { next: { revalidate: 0 } });
   const topics = await response.json();
   console.log(topics); // { id, title, message }
 
   return (
-    <html
-      lang="ko"
-      data-scroll-behavior="smooth"
-      // className={`${geistSans.variable} ${geistMono.variable}`}
-    >
+    <html lang="ko" data-scroll-behavior="smooth">
       <body>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid d-flex justify-content-between">
@@ -58,17 +41,7 @@ export default async function RootLayout({ children }) {
         <main>
           {children}
           <hr />
-          <div className="d-flex gap-1">
-            <Link className="btn btn-primary" href="/create">
-              Create
-            </Link>
-            <Link className="btn btn-secondary" href="/update">
-              Update
-            </Link>
-            <Link className="btn btn-danger" href="/delete">
-              Delete
-            </Link>
-          </div>
+          <Controls />
         </main>
         <Script src="/main.js" strategy="afterInteractive" />
       </body>
